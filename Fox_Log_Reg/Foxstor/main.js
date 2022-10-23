df10da48becd5fe53b583daee5eb8bed971f3c60
SprinFoxAlasDHAMF <volpinoe@gmail.com>
2022-10-23 22:35:11 +0200

    carrusel

--- Fox_Log_Reg/Foxstor/main.js
+++ Fox_Log_Reg/Foxstor/main.js
@@ -1,24 +1,25 @@
  const fila = document.querySelector('.contenedor-carusel');
- const peliculas = document.querySelector('.pelicula');
+ const peliculas = document.querySelectorAll('.pelicula');
+
  const flechaIzquieda = document.getElementById('flecha-izquierda');
  const flechaDerecha = document.getElementById('flecha-derecha');
 
  flechaDerecha.addEventListener('click',() => {
-     fila.scrollLeft += fila.offsetWidth
-
-     const indicadorActivo = document.querySelector('.indicadores .activo');
-     if(indicadorActivo.nextSibling){
-         indicadorActivo.nextSibling.classList.add('avtivo');
-         indicadorActivo.classList.remove('activo');
-     }
+     fila.scrollLeft += fila.offsetWidth;
  });
+
  flechaIzquieda.addEventListener('click',() => {
-    fila.scrollLeft -= fila.offsetWidth
+    fila.scrollLeft -= fila.offsetWidth;
 });
 
-const numeroPaginas = Math.ceil(peliculas.length / 5);
-for(let i = 0; i < numeroPaginas; i++){
-    const indicador = document.createElement('button');
-    document.querySelector('.indicadores').appendChild(indicador);
-   
-}
+const numeroPaginas = Math.ceil(peliculas.length /5);
+
+peliculas.forEach((pelicula) => {
+    pelicula.addEventListener('mouseenter', (e) =>{
+        const elemento = e.currentTarget;
+        setTimeout(() => {
+            pelicula.forEach(pelicula => pelicula.classList.remove('hover'));
+            elemento.classList.add('hover');
+        }, 300);
+    });
+});
